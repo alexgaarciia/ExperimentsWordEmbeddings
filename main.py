394393b0c4d6558d7593457288a671e54205606b
alexgaarciia <al.garcia636@gmail.com>
2023-11-22 21:30:28 -0500

    Started task 1 by loading the pretrained embedding model

---
+++ main.py
@@ -0,0 +1,10 @@
+# Import necessary libraries/modules
+import gensim.downloader
+
+# ----------------------------------------------------------------------------------------------------------------------
+# TASK 1
+# ----------------------------------------------------------------------------------------------------------------------
+# Load the pretrained embedding model:
+model = gensim.downloader.load("word2vec-google-news-300")
+
+#
