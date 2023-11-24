3a0cd7dbb232069ca39196d4856186e3eb0e03fd
Lucía Cordero Sánchez <lucia_corsan@yahoo.es>
2023-11-23 20:57:59 -0500

    creation of functions for model evaluation, computing accuracy and save to CSV

--- main.py
+++ main.py
@@ -15,62 +15,82 @@ print(dataset)
 # Prepare the output data:
 output_data = []
 
-# Iterate through each row in the dataset
-for index, row in dataset.iterrows():
-    # Extract the question and answer words from the current row
-    question_word = row['question']
-    answer_word = row['answer']
+def model_evaluator(model, dataset):
+    # Iterate through each row in the dataset
+    for index, row in dataset.iterrows():
+        # Extract the question and answer words from the current row
+        question_word = row['question']
+        answer_word = row['answer']
 
-    # Generate a list of guess words from the current row
-    guess_words = [row[str(i)] for i in range(4)]
+        # Generate a list of guess words from the current row
+        guess_words = [row[str(i)] for i in range(4)]
 
-    # Check if the question word is in the model
-    if question_word in model:
-        # Initialize a list to store similarities between question and guess words
-        similarities = []
+        # Check if the question word is in the model
+        if question_word in model:
+            # Initialize a list to store similarities between question and guess words
+            similarities = []
 
-        # Loop through each guess word
-        for guess_word in guess_words:
-            # Check if the guess word is in the model
-            if guess_word in model:
-                # Calculate the similarity between question and guess word using the similarity method from Gensim
-                similarity = model.similarity(question_word, guess_word)
-                # Store the guess word and its similarity score
-                similarities.append((guess_word, similarity))
+            # Loop through each guess word
+            for guess_word in guess_words:
+                # Check if the guess word is in the model
+                if guess_word in model:
+                    # Calculate the similarity between question and guess word using the similarity method from Gensim
+                    similarity = model.similarity(question_word, guess_word)
+                    # Store the guess word and its similarity score
+                    similarities.append((guess_word, similarity))
 
-        # Determine the best guess based on the highest similarity score
-        if similarities:
-            best_guess, _ = max(similarities, key=lambda x: x[1])
-            # Label as "correct" if the best guess matches the answer word
-            label = 'correct' if best_guess == answer_word else 'wrong'
+            # Determine the best guess based on the highest similarity score
+            if similarities:
+                best_guess, _ = max(similarities, key=lambda x: x[1])
+                # Label as "correct" if the best guess matches the answer word
+                label = 'correct' if best_guess == answer_word else 'wrong'
+            else:
+                # If no valid guesses, set best guess to None and label as "guess"
+                best_guess = None
+                label = 'guess'
         else:
-            # If no valid guesses, set best guess to None and label as "guess"
+            # If question word is not in the model, no guess can be made
             best_guess = None
             label = 'guess'
-    else:
-        # If question word is not in the model, no guess can be made
-        best_guess = None
-        label = 'guess'
 
-    # Append the results to the output data
-    output_data.append([question_word, answer_word, best_guess, label])
+        # Append the results to the output data
+        output_data.append([question_word, answer_word, best_guess, label])
+    return output_data
 
 # Convert the output data to a DataFrame and save the output to a CSV file:
 output_df = pd.DataFrame(output_data, columns=['question-word', 'answer-word', 'guess-word', 'label'])
 output_df.to_csv('word2vec-google-news-300-details.csv', index=False)
 
 # In order to create the "analysis.csv" file, some steps must be followed:
-# 1. Count the number of correct (C) and non-guess labels (V):
-C = (output_df['label'] == 'correct').sum()
-V = (output_df['label'] != 'guess').sum()
+def compute_accuracy(output_df):
+    # 1. Count the number of correct (C) and non-guess labels (V):
+    C = (output_df['label'] == 'correct').sum()
+    V = (output_df['label'] != 'guess').sum()
 
-# 2. Calculate the accuracy:
-if V:
-    accuracy = C/V
-else:
-    accuracy = 0
+    # 2. Calculate the accuracy:
+    if V:
+        accuracy = C/V
+    else:
+        accuracy = 0
+    return ("The accuracy of this model is: ", accuracy)
+
 
 # 3. Create a DataFrame and save to csv:
+analysis_df = {}
+def save_to_CSV(analysis_df, model_name, model, correct_labels, answered_questions, accuracy):
+    new_entry = {
+        'model_name': model_name,
+        'vocabulary_size': len(model.key_to_index),
+        'correct_labels': correct_labels,
+        'answered_questions': answered_questions,
+        'accuracy': accuracy
+    }
+    analysis_df[model_name] = new_entry
+    return analysis_df
+
+"""
+analysis_df = add_model_analysis(analysis_df, 'word2vec-google-news-300', model, C, V, accuracy)
+
 analysis_df = pd.DataFrame({
     'model_name': ['word2vec-google-news-300'],
     'vocabulary_size': [len(model.key_to_index)],
@@ -78,4 +98,6 @@ analysis_df = pd.DataFrame({
     'answered_questions': [V],
     'accuracy': [accuracy]
 })
+"""
+
 analysis_df.to_csv('analysis.csv', index=False)
